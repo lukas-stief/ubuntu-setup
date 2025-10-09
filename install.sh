920f91ab86bb6a70da7626a2baa545dde0819bdf
Lukas Stief <stiefluk@gmail.com>
2025-10-09 17:41:50 +0200

    install usb-creator-gtk to allow creating bootable usb sticks

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 # -o pipefail the return value of a pipeline is the status of the last command to
 set -eo pipefail
 
+echo "Updating package lists..."
+sudo apt update
+echo "Package lists updated."
+
 echo "Applying gnome settings..."
 sh scripts/gnome-settings.sh
 echo "Gnome settings applied."
+
+echo "Installing usb-creator-gtk..."
+sh scripts/usb-creator-gtk-install.sh
+echo "usb-creator-gtk installed."
---
+++ scripts/usb-creator-gtk-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install usb-creator-gtk if not already installed
+# usb-creator-gtk is the default USB creator tool in Ubuntu
+# this allows to easily create bootable USB sticks from ISO files
+sudo apt install usb-creator-gtk
